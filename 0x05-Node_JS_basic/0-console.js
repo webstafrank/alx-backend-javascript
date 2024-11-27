f2355eaa573e5bd37b54cab0b0e1d23055d2d814
webstafrank <franklyndenish@gmail.com>
2024-11-27 01:25:23 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// 0-console.js
+function displayMessage(message) {
+  console.log(message); // Print the message to STDOUT
+}
+
+module.exports = displayMessage; // Export the function for use in other files
+
